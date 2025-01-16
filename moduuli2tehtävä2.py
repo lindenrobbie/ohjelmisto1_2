177cb188422553b49148d4498fd973b38b026385
robbi <robbie.linden@metropolia.fi>
2025-01-16 13:29:32 +0200

    Moduuli 1, tehtävä 2

---
+++ moduuli2tehtävä2.py
@@ -0,0 +1,9 @@
+import math
+
+print("Tässä on ympyrän säteen pinta-ala laskin.")
+radius = input('Mikä on ympyrän säde? ')
+radius_value = float(radius)
+
+area = (math.pi * (radius_value **2))
+
+print(f"Ympyrän pinta-ala on: {area:.4f}")
