3fbd947a29e48b69a3fc049f2a6952792d31ab0e
robbi <robbie.linden@metropolia.fi>
2025-01-17 10:29:58 +0200

    Moduuli 2, tehtävä 3

---
+++ moduuli2tehtävä3.py
@@ -0,0 +1,16 @@
+w = int(input('Mikä on kannan pituus? '))
+
+h = int(input('Mikä on korkeuden pituus? '))
+
+a = w * h
+c = 2 * w + 2 * h
+
+print(f"Suorakulmion pinta-ala on:  {a}m^2")
+print(f"Suorakulmion piiri on:  {c}m")
+
+
+
+
+
+
+
