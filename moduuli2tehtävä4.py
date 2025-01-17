e9fdd2f3d4faaf226849d8cea7946a6c57c7f4ad
robbi <robbie.linden@metropolia.fi>
2025-01-17 11:22:45 +0200

    Moduuli 2, tehtävä 5

---
+++ moduuli2tehtävä4.py
@@ -0,0 +1,14 @@
+
+print()
+value1 = int(input('Kirjoita kokonaisluvun 1:sen arvo: '))
+value2 = int(input('Kirjoita kokonaisluvun 2:sen arvo: '))
+value3 = int(input('Kirjoita kokonaisluvun 3:sen arvo: '))
+
+sum = value1 + value2 + value3
+product = value1 * value2 * value3
+average = sum / 3
+
+print()
+print(f"Lukujen summa on {sum}.")
+print(f"Lukujen tulo on {product}.")
+print(f"Lukujen keskiarvo on {average}.")
