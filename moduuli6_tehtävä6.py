3a2948b6108a6434d418d6d755801292ea9ca00a
lindenrobbie <robbie.linden@metropolia.fi>
2025-01-31 00:00:32 +0200

    Update moduuli6_tehtävä6.py

--- moduuli6_tehtävä6.py
+++ moduuli6_tehtävä6.py
@@ -2,21 +2,21 @@ print("")
 
 from math import pi
 
-def pizza_value(halkaisija, hinta):
-    pinta_ala = (halkaisija / 2) ** 2 * pi
-    return (hinta / pinta_ala)
+def pizza_value(diameter, price):
+    area = (diameter / 2) ** 2 * pi
+    return (price / area)
 
 
 def main():
     pizzas = {}
 
     for i in range(1, 3):
-        halkaisija = int(input(f"Pizza {i}:n halkaisija: "))
+        diameter = int(input(f"Pizza {i}:n halkaisija: "))
         print("")
-        hinta = int(input(f"Pizza {i}:n hinta: "))
+        price = int(input(f"Pizza {i}:n hinta: "))
         print("")
 
-        pizzas[pizza_value(halkaisija, hinta)] = i
+        pizzas[pizza_value(diameter, price)] = i
 
     return print(f"Pizza {pizzas[min(pizzas)]} on better bang for ya buck")
 
