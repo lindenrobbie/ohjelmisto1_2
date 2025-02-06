cc0102a1671d1e52165e6dcfb7339694322033c1
lindenrobbie <robbie.linden@metropolia.fi>
2025-02-06 11:58:27 +0200

    Create moduuli8_tehtävä1.py
    Tässä on moduuli 8 tehtävä 1

---
+++ moduuli8_tehtävä1.py
@@ -0,0 +1,41 @@
+import mysql.connector
+
+print("")
+
+yhteys = mysql.connector.connect(
+         host='localhost',
+         port= 3306,
+         database='lentokonepeli',
+         user='robbie',
+         password='lol',
+         autocommit=True,
+         collation='utf8mb3_unicode_ci')
+
+def main():
+
+    while True:
+
+        print('Please remember to add semicolon after SQL query.')
+        tablerequest = input('Syötä tähän ICAO koodi: ')
+        sql = f"SELECT airport.name FROM airport WHERE airport.ident = '{tablerequest}';"
+        print("")
+
+        if sql == '':
+            break
+
+        else:
+
+            with yhteys.cursor() as kursori:
+                kursori.execute(sql)
+
+                tulos = kursori.fetchall()
+
+                if kursori.rowcount >0:
+                    for rivi in tulos:
+                        print(f'Output: {rivi[0]}')
+                        print("")
+                else:
+                    print('No results found.')
+                    print("")
+
+main()
