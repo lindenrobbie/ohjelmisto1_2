687e568d1d45523e2e8bb6763679eca145f7cf13
Robbie Linden <robbie.linden@metropolia.fi>
2025-04-10 17:07:18 +0300

    moduuli 13 tunti shitit

--- moduuli13tunti.py
+++ moduuli13tunti.py
@@ -18,4 +18,4 @@ def summa():
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
 
-#plspush
+#plspushp
--- moduuli13tunti2.py
+++ moduuli13tunti2.py
@@ -41,4 +41,4 @@ if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
 
 
-#plspush
+#plspushp
